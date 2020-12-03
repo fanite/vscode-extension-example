d9ff9ad1a07d9b5d8f3425c11f55b94931ed93d5
Peyton Wong <bellong@vip.qq.com>
2020-12-04 01:44:29 +0800

    dsfsf

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-import * as ADB from "appium-adb";
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "autotest" is now active!');
-
-  const adb = await ADB.createADB();
-  console.log(await adb.getPIDsByName("com.android.phone"));
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
